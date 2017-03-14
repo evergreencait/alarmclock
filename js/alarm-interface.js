b42aad52812f4d3f9fdbd958bd040cd2a78d5815
Caitlin Hines and Keely Glenn <caitlin+keely@>
2017-03-14 14:09:14 -0700

    pretty code pretty cool

--- js/alarm-interface.js
+++ js/alarm-interface.js
@@ -1,35 +1,17 @@
 var Alarm = require('./../js/alarm.js').alarmModule;
 
 $(document).ready(function(){
-  $('#time').text(moment().format('H:mm:ss'));
-  $('#alarm-set').submit(function(event) {
-    event.preventDefault();
-    var hourSet= $('#hour').val();
-    var minSet = $('#min').val();
-      var nowHour = moment().format('H');
-      // var nowMin = moment().format('mm');
-      var result;
-      if (moment(hourSet).isSameOrAfter(nowHour)) {
-        result = true;
-      }
-      else {
-        result = false;
-      }
-
-
-
-  $("#result").text(result);
-  });
-
   setInterval(function time() {
     $('#time').text(moment().format('H:mm:ss'));
   } , 1000)
+  $('#alarm-set').submit(function(event) {
+    event.preventDefault();
 
-  //every second
-  setInterval(function() {
-    if(moment().format("H") == $('#hour').val() && moment().format("mm") == $('#min').val())
-    {
-      $(".test").toggle();
-    }
-  }, 1000)
+    setInterval(function() {
+      if(moment().format("H") == $('#hour').val() && moment().format("mm") == $('#min').val())
+      {
+        $(".test").toggle();
+      }
+    }, 1000)
+  });
 });
